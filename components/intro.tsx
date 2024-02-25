e6ece1247532890f6e9bec56f669f012b2e4dd39
Indranil-Sarmah <isharma055@gmail.com>
2024-02-26 00:21:19 +0530

    Added Typewritter effect

--- components/intro.tsx
+++ components/intro.tsx
@@ -4,12 +4,14 @@ import React from 'react'
 import Image from 'next/image';
 import { animate, motion } from 'framer-motion'
 import GitHubCalendar from 'react-github-calendar';
+import { Typewriter } from 'react-simple-typewriter'
 
 const Intro = () => {
 
   const localImage = require('../public/INDARANIL_PHOTO.jpg').default;
+  {console.log(window.innerWidth)}
   return (
-    <section>
+    <section className='mb max-w-[50rem] text-center sm:mb-0'>
       {/* image and icon section */}
       <div className='flex items-center justify-center '>
         <motion.div className=" absolute -z-1 mb-8 bg-opacity-10 hidden md:block" style={{color:"#a39e9e"}} initial={{ opacity: 0, scale: 0 }} animate={{ opacity: 1, scale: 1 }} transition={{ type: "tween", duration: 0.2 }}>
@@ -35,17 +37,29 @@ const Intro = () => {
 
       {/* Intro text */}
       <motion.h4
-        className="mb-10 mt-4 text-center px-12 text-2xl font-medium !leading-[1.5] sm:text-1xl"
+        className="mb-10 mt-4 text-center px-12 text-2xl font-medium !leading-[1.5] sm:text-1xl sm:text-justify-center"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
-        <span className="font-bold">Hello, I am Indranil.</span> I am a{" "}
-        <span className="font-bold">front-end developer</span> with{" "}
-        <span className="underline font-bold">2 years</span> of experience. I enjoy
-        building <span className="italic underline">sites & web-apps</span>. <br />My focus is with{" "}
+        <span className="font-extrabold block"><Typewriter
+            words={['Hello', 'Hi', 'Namaste']}
+            loop={Infinity}
+            cursor
+            cursorStyle='|'
+            typeSpeed={60}
+            deleteSpeed={50}
+            
+          />
+        <br/></span>
+        <span> I am {" "}<span className='font-extrabold'>Indranil.</span></span><br/>
+        
+        I am a{" "}
+        <span className="font-bold">Front-end developer</span> with{" "}
+        <span className="underline font-bold">2 years</span> of experience.<br/>Love to work with{" "}
         <span className="underline font-bold">ReactJS </span>
         <span> & Currently learning NextJS.</span>
       </motion.h4>
+    
 
     </section>
   )
