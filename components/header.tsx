71ceae7327c2cb7b665fe42e53b9b7098ededca2
Indranil-Sarmah <isharma055@gmail.com>
2024-02-25 22:45:39 +0530

    changes related git hub chart background

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React from 'react';
-import {  motion } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { links } from '@/lib/data';
 import Link from "next/link";
 
@@ -17,7 +17,7 @@ function Header() {
                 <ul className='flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5'>
                     {
                         links.map((link) => {
-                            return (<motion.li className="h-3/4 flex items-center justify-center" initial={{y:-100,opacity:0}} animate={{y:0,opacity:1}} key={link.hash}>
+                            return (<motion.li className="h-3/4 flex items-center justify-center" initial={{ y: -100, opacity: 0 }} animate={{ y: 0, opacity: 1 }} key={link.hash}>
                                 <Link className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition" href={link.hash}>{link.name}</Link>
                             </motion.li>)
                         })
--- components/intro.tsx
+++ components/intro.tsx
@@ -2,23 +2,51 @@
 
 import React from 'react'
 import Image from 'next/image';
-import {animate, motion} from 'framer-motion'
+import { animate, motion } from 'framer-motion'
+import GitHubCalendar from 'react-github-calendar';
 
 const Intro = () => {
 
   const localImage = require('../public/INDARANIL_PHOTO.jpg').default;
   return (
     <section>
-      <div className='flex items-center justify-center'>
-      <div className='relative'>
-        <motion.div initial={{opacity:0,scale:0}} animate={{opacity:1, scale:1}}>
-          <Image alt="profile_pic" src={localImage} width={192} height={192} quality={100} priority={true} className='h-21 w-24 object-cover rounded-full border-[0.35rem] border-white shadow-xl'></Image>
+      {/* image and icon section */}
+      <div className='flex items-center justify-center '>
+        <motion.div className=" absolute -z-1 mb-8 bg-opacity-10 hidden md:block" style={{color:"#a39e9e"}} initial={{ opacity: 0, scale: 0 }} animate={{ opacity: 1, scale: 1 }} transition={{ type: "tween", duration: 0.2 }}>
+          <GitHubCalendar username="Indranil-Sarmah" hideTotalCount={true} hideColorLegend={true} />
         </motion.div>
-        <span className='text-2xl absolute bottom-0 right-0'>
-          👋🏼
-        </span>
+        <div className='relative'>
+          <motion.div initial={{ opacity: 0, scale: 0 }} animate={{ opacity: 1, scale: 1 }} transition={{ type: "tween", duration: 0.2 }}>
+            <Image alt="profile_pic" src={localImage} width={192} height={192} quality={100} priority={true} className='h-21 w-24 object-cover rounded-full border-[0.35rem] border-white shadow-xl'></Image>
+          </motion.div>
+          <motion.span className='text-2xl absolute bottom-0 right-1' initial={{ opacity: 0, scale: 0, rotate: 0 }}
+            // Add rotate property for waving motion
+            animate={{
+              opacity: 1,
+              scale: 1,
+              rotate: [0, -20, 0, 35, 0], // Rotate back and forth for waving effect
+            }}
+            transition={{ type: 'spring', stiffness: 125, delay: 0.1, duration: 0.7 }}>
+            👋🏼
+          </motion.span>
+
+        </div>
       </div>
-      </div>
+
+      {/* Intro text */}
+      <motion.h4
+        className="mb-10 mt-4 text-center px-12 text-2xl font-medium !leading-[1.5] sm:text-1xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I am Indranil.</span> I am a{" "}
+        <span className="font-bold">front-end developer</span> with{" "}
+        <span className="underline font-bold">2 years</span> of experience. I enjoy
+        building <span className="italic underline">sites & web-apps</span>. <br />My focus is with{" "}
+        <span className="underline font-bold">ReactJS </span>
+        <span> & Currently learning NextJS.</span>
+      </motion.h4>
+
     </section>
   )
 }
